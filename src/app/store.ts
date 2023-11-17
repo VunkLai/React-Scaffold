0636dfcf6d1a9e40797520a578adc95702a37af9
vunk.lai <vunk.lai@zyxel.com.tw>
2023-11-17 11:31:42 +0800

    feat: add middlewares to redux toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  Action,
+  Dispatch,
+  MiddlewareAPI,
+  configureStore,
+} from "@reduxjs/toolkit";
 import appReducer from "../features/app/appSlice";
 import counterReducer from "../features/counter/counterSlice";
+
+const middlewares = [
+  (store: MiddlewareAPI) => (next: Dispatch) => (action: Action) => {
+    console.log("Dispatching", JSON.stringify(action));
+    console.log("Current State", JSON.stringify(store.getState()));
+    const result = next(action);
+    console.log("Next State", JSON.stringify(store.getState()));
+    return result;
+  },
+];
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewares),
+  devTools: false,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
